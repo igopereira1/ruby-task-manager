28d8907c152e859da9782e5022d58aa2ae72d7ce
igopereira1 <igopereira1@gmail.com>
2025-12-17 09:58:36 -0300

    feat: add User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_12_17_022108) do
+ActiveRecord::Schema[7.1].define(version: 2025_12_17_030411) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.boolean "completed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
