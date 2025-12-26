d1da9fd6d7982731963d15f2d66633753946e5f5
igopereira1 <igopereira1@gmail.com>
2025-12-26 18:28:00 -0300

    feat: add signup route POST /api/v1/users to UsersController

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post 'login', to: 'auth#login'
+      post 'users', to: 'users#create'
       resources :tasks
     end
   end
