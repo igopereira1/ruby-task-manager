4d5dbf6f978a89cf8b9dce1d2db87e2d61d8dcf7
igopereira1 <igopereira1@gmail.com>
2025-12-26 17:01:31 -0300

    refactor: extract task lookup to before_action in TasksController

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,55 +1,47 @@
 class Api::V1::TasksController < ApplicationController
-      # GET /api/v1/tasks
-      def index
-        tasks = current_user.tasks
-        render json: tasks
-      end
+  before_action :set_task, only: %i[show update destroy]
 
-      # GET /api/v1/tasks/:id
-      def show
-        task = current_user.tasks.find(params[:id])
-        render json: task
-      rescue ActiveRecord::RecordNotFound
-        render json: { error: "Task not found" }, status: :not_found
-      end
+  def index
+    render json: current_user.tasks
+  end
 
-      # POST /api/v1/tasks
-      def create
-        task = current_user.tasks.new(task_params)
-        if task.save
-          render json: task, status: :created
-        else
-          render json: { errors: task.errors.full_messages }, status: :unprocessable_entity
-        end
-      end
+  def show
+    render json: @task
+  end
 
-      # PATCH/PUT /api/v1/tasks/:id
-      def update
-        task = current_user.tasks.find(params[:id])
-        if task.update(task_params)
-          render json: task, status: :ok
-        else
-          render json: { errors: task.errors.full_messages }, status: :unprocessable_entity
-        end
-      rescue ActiveRecord::RecordNotFound
-        render json: { error: "Task not found" }, status: :not_found
-      end
+  def create
+    task = current_user.tasks.new(task_params)
+    if task.save
+      render json: task, status: :created
+    else
+      render json: { errors: task.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
 
-      # DELETE /api/v1/tasks/:id
-      def destroy
-        task = current_user.tasks.find(params[:id])
-        task.destroy!
-        head :no_content
-      rescue ActiveRecord::RecordNotFound
-        render json: { error: "Task not found" }, status: :not_found
-      end
+  def update
+    if @task.update(task_params)
+      render json: @task
+    else
+      render json: { errors: @task.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
 
-      private
+  def destroy
+    @task.destroy!
+    head :no_content
+  end
 
-      def task_params
-        params.permit(:title, :completed)
-      end
+  private
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Task not found' }, status: :not_found
+  end
+
+  def task_params
+    params.permit(:title, :completed)
+  end
 end
-
-
-
